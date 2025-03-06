ddf20ef8854b0b0ef0c2967c8f8347f014c957c4
Bashar Saeed <bwasaeed@gmail.com>
2025-03-07 02:33:36 +0300

    define get_char_count

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import get_num_words
+from stats import get_num_words, get_char_count
 
 
 def get_book_text(path_to_file: str) -> str:
@@ -11,6 +11,8 @@ def main() -> None:
     book_text: str = get_book_text(path_to_file)
     num_words: int = get_num_words(book_text)
     print(f"{num_words} words found in the document")
+    num_chars: dict = get_char_count(book_text)
+    print(num_chars)
 
 
 main()
--- stats.py
+++ stats.py
@@ -1,2 +1,13 @@
 def get_num_words(text: str) -> int:
     return len(text.split())
+
+
+def get_char_count(text: str) -> dict[str, int]:
+    text = text.lower()
+    char_count = {}
+    for char in text:
+        if char in char_count:
+            char_count[char] += 1
+        else:
+            char_count[char] = 1
+    return char_count
