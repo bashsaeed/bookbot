bd089d8d236cfb5d4973daa3266946481f537fbb
Bashar Saeed <bwasaeed@gmail.com>
2025-03-07 02:25:38 +0300

    define count_words

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 def get_book_text(path_to_file: str) -> str:
     with open(path_to_file, "r") as file:
         return file.read()
+
+
+def count_words(text: str) -> int:
+    return len(text.split())
+
+
 def main() -> None:
     path_to_file = "books/frankenstein.txt"
     book_text: str = get_book_text(path_to_file)
+    num_words: int = count_words(book_text)
+    print(f"{num_words} words found in the document")
+
+
 main()
